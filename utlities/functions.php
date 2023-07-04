9283d80875292a5cf3af9a729f0c360b7c5b217b
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-04 15:56:10 +0200

    addded new function to generate password from ascii characters

--- utlities/functions.php
+++ utlities/functions.php
@@ -1,10 +1,10 @@
 <?php 
-    
-    
-    
-    
-    
-    
-    
-    
+    function generatePassword($length) {
+        $password = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomAscii = random_int(33, 126); // ASCII range from '!' to '~'
+            $password .= chr($randomAscii);
+        }
+        return $password;
+    }
 ?>
