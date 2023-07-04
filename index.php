8132407b377e4ed212088c922e69189422d7a948
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-04 16:33:30 +0200

    added  new function to geenrate password with randoma scci characters

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
-
+    include __DIR__ . '/utilities/functions.php';
+    
+    $passwordLength = isset($_GET['passwordLength']) ? intval($_GET['passwordLength']) : "";
+    $generatedPassword = "";
+    
+    if (!empty($passwordLength)) {
+        $generatedPassword = generatePassword($passwordLength);
+        if (empty($generatedPassword)) {
+            $generatedPassword = "Failed to generate password. Please try again.";
+        }
+    } else {
+        $generatedPassword = "Select a length to generate a password.";
+    }
 ?>
 
 <!DOCTYPE html>
@@ -10,6 +22,12 @@
     <title>Password Generator</title>
 </head>
 <body>
-    
+    <form method="GET" action="./index.php">
+        <label for="passwordLength">Password Length:</label>
+        <input type="number" name="passwordLength" id="passwordLength" min="1" max="100" value="10">
+        <button type="submit">Generate Password</button>
+    </form>
+
+    <p><?php echo $generatedPassword; ?></p>
 </body>
 </html>
