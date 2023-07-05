bdb320143bdc733f22f42f652f3626e4481f979f
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-05 08:08:18 +0200

    styled entire page, added new content and button role

--- index.php
+++ index.php
@@ -8,27 +8,49 @@
     if (!empty($passwordLength)) {
         $generatedPassword = generatePassword($passwordLength);
         if (empty($generatedPassword)) {
-            $generatedPassword = "Failed to generate password. Please try again.";
+            $generatedPassword = "Errore nella creazione della password. Riprova!";
         }
     } else {
-        $generatedPassword = "Select a length to generate a password.";
+        $generatedPassword = "Nessun parametro valido inserito";
+    }
+
+    if (isset($_GET['clear']) && $_GET['clear'] === 'true') {
+        $generatedPassword = "Nessun parametro valido inserito";
     }
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en, it">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Password Generator</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" 
+        rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    
 </head>
 <body>
-    <form method="GET" action="./index.php">
-        <label for="passwordLength">Password Length (max: 50):</label>
-        <input type="number" name="passwordLength" id="passwordLength" min="1" max="50" value="10">
-        <button type="submit">Generate Password</button>
-    </form>
 
-    <p><?php echo $generatedPassword; ?></p>
+    <h1>Strong Password Generator</h1>
+    <h2>Genera una password sicura</h2>
+
+    <main>
+        <div class="alert alert-info" role="alert">
+            <p><?php echo $generatedPassword; ?></p>
+        </div>
+
+        <section>
+            <form method="GET" action="./index.php">
+                <label for="passwordLength">Lunghezza Password [max: 50]:</label>
+                <input type="number" name="passwordLength" id="passwordLength" min="1" max="50" value="10">
+                <div class="buttons-form">
+                    <button type="submit" class="btn btn-primary">Generate Password</button>
+                    <button type="submit" class="btn btn-secondary" name="clear" value="true">Clear</button>
+                </div>
+            </form>
+        </section>
+    </main>
 </body>
 </html>
